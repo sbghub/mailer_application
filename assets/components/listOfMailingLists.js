4a401e89471f5992ebeb4af871a41d9a25095698
Somak Bhattacharya <pb4301@att.com>
2017-08-24 12:25:36 -0400

    got list to add items dynamically

--- assets/components/listOfMailingLists.js
+++ assets/components/listOfMailingLists.js
@@ -1,20 +1,74 @@
 import React from "react";
-import ReactDOM from "react-dom";
+
 
 let ListOfMailingLists = React.createClass({
-  render() {
+
+  getInitialState: function () {
     return (
-      <div name="mailingList">
-        <button name="add">Add Email List</button>
-        <ul name="listOfEmailLists">
-          <li name="emailList">
-            name 1
-            <button name="delete">Delete</button>
-          </li>
+      { mailingLists: {} }
+    )
+  },
+
+  addItem: function (newItem) {
+    let timestamp = (new Date()).getTime();
+    this.state.mailingLists['fruit-' + timestamp] = newItem;
+    this.setState({ mailingLists: this.state.mailingLists });
+  },
+
+  render: function () {
+    return (
+      <div>
+        <AddListForm addItem={this.addItem} />
+        <MailingList mailingLists={this.state.mailingLists} />
+      </div>
+    );
+  }
+});
+
+
+let MailingList = React.createClass({
+
+  render: function () {
+    return (
+      <div>
+        <ul>
+          {
+            Object.keys(this.props.mailingLists).map(function (key) {
+              return <li>{this.props.mailingLists[key]}</li>
+            }.bind(this))
+          }
         </ul>
       </div>
+    );
+  }
+});
+
+
+let AddListForm = React.createClass({
+
+  createList: function (e) {
+    e.preventDefault();
+    let mailingList = this.refs.listName.value;
+    if (typeof mailingList === 'string' && mailingList.length > 0) {
+      this.props.addItem(mailingList);
+      this.refs.listForm.reset();
+    }
+  },
+
+  render: function () {
+    return (
+      <form ref="listForm" onSubmit={this.createList}>
+        <button type="submit" >Add Mailing List</button>
+        <div>
+          <div>
+            List Name
+            <input type="text" placeholder="ex. Incubate" ref="listName" />
+          </div>
+        </div>
+      </form>
     )
   }
-})
+});
+
 
 export default ListOfMailingLists;
