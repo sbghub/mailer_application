02d942ac00d1dd746b53b7fc62c41bd4609d2fc5
Somak Bhattacharya <pb4301@att.com>
2017-08-24 16:16:41 -0400

    changed state of mailinglist to list instead of object

--- assets/components/listOfMailingLists.js
+++ assets/components/listOfMailingLists.js
@@ -2,17 +2,75 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 let ListOfMailingLists = React.createClass({
+
+  getInitialState() {
+    return (
+      { mailingLists: [] }
+    )
+  },
+
+  addItem(newItem) {
+    let newList = this.state.mailingLists;
+    newList.push(newItem);
+    this.setState({ mailingLists: newList });
+  },
+
   render() {
     return (
-      <div name="mailingList">
-        <button name="add">Add Email List</button>
-        <ul name="listOfEmailLists">
-          <li name="emailList">
-            name 1
-            <button name="delete">Delete</button>
-          </li>
+      <div>
+        <AddListForm addItem={this.addItem} />
+        <GroupList mailingLists={this.state.mailingLists} />
+      </div>
+    );
+  }
+});
+
+
+let GroupList = React.createClass({
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {
+            Object.keys(this.props.mailingLists).map(function (item, index) {
+              return (
+                <div>
+                  <li>{this.props.mailingLists[index]}</li>
+                  <button>Delete</button>
+                </div>
+              )
+            }.bind(this))
+          }
         </ul>
       </div>
+    );
+  }
+});
+
+
+let AddListForm = React.createClass({
+
+  createList(e) {
+    e.preventDefault();
+    let mailingList = this.refs.listName.value;
+    if (typeof mailingList === 'string' && mailingList.length > 0) {
+      this.props.addItem(mailingList);
+      this.refs.listForm.reset();
+    }
+  },
+
+  render() {
+    return (
+      <form ref="listForm" onSubmit={this.createList}>
+        <button type="submit" >Add Mailing List</button>
+        <div>
+          <div>
+            List Name
+            <input type="text" placeholder="ex. Incubate" ref="listName" />
+          </div>
+        </div>
+      </form>
     )
   }
 })
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
+pip install -r requirements.txt
+
+npm install
+
 ./node_modules/.bin/webpack --config webpack.config.js
 
 ./manage.py runserver
