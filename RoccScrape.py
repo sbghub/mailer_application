f006b70450b7fdaa3b05c10d9fe10b0c14cf6df2
Somak Bhattacharya <pb4301@att.com>
2017-08-30 16:54:26 -0400

    looping through each sheet and returning each field

--- RoccScrape.py
+++ RoccScrape.py
@@ -7,14 +7,21 @@ from collections import OrderedDict
 
 def open_file(path):
 
-	fname = join(dirname(dirname(abspath(__file__))), 'Documents', 'roccStuff.xlsx')
+	fname = join(dirname(dirname(abspath(__file__))), 'mailer_application', 'roccStuff.xlsx')
 
 	xl_workbook = xlrd.open_workbook(fname)
 	book = xlrd.open_workbook("roccStuff.xlsx")
 	sheet_names = xl_workbook.sheet_names()
-	print("SHEET NAMES", sheet_names)
-	xl_sheet = xl_workbook.sheet_by_name(sheet_names[0])
-	checkboxes = xl_workbook.sheet_by_name(sheet_names[1])
+	
+	for sheet in sheet_names:
+		print("SHEET NAMED", sheet)
+		sheet = xl_workbook.sheet_by_name(sheet)
+		num_cols = sheet.ncols
+
+
+		for col in range(num_cols):
+			print("cell param", sheet.cell(0, col))
+		print("-----end-sheet-----"*3)
 
 if __name__ == "__main__":
     path = "roccStuff.xlsx"
